199c1a097a590928a8cd15ff41578edb784e7c10
maruhxn <maruhxn@aswemake.com>
2026-02-18 13:45:38 +0900

    DomainService 애노테이션 추가

---
+++ core/src/main/kotlin/com/aswemake/my_study/common/DomainService.kt
@@ -0,0 +1,8 @@
+package com.aswemake.my_study.common
+
+import org.springframework.stereotype.Component
+
+@Component
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DomainService()

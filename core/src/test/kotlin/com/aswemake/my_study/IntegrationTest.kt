946d614fd80ca73bf3d3cf87cb1fd28b64d9d1c8
maruhxn <maruhxn@aswemake.com>
2026-02-18 15:27:53 +0900

    IntegrationTest에 테스트 Configuration 응집 (@SpringBootTest)

--- core/src/test/kotlin/com/aswemake/my_study/IntegrationTest.kt
+++ core/src/test/kotlin/com/aswemake/my_study/IntegrationTest.kt
@@ -2,9 +2,7 @@ package com.aswemake.my_study
 
 import com.aswemake.my_study.common.TimeProvider
 import com.ninjasquad.springmockk.MockkBean
-import io.mockk.clearAllMocks
 import jakarta.persistence.EntityManager
-import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
@@ -18,9 +16,9 @@ import org.springframework.transaction.annotation.Transactional
 @Import(TestcontainersConfiguration::class)
 @TestPropertySource(properties = ["spring.jpa.hibernate.ddl-auto=create-drop"])
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-class IntegrationTest {
+abstract class IntegrationTest {
 
-    @SpringBootApplication
+    @SpringBootApplication // 패키지가 com.aswemake.my_study 스캔 → scanBasePackages 불필요
     class Config
 
     @Autowired
@@ -28,9 +26,4 @@ class IntegrationTest {
 
     @MockkBean
     protected lateinit var timeProvider: TimeProvider
-
-    @BeforeEach
-    protected fun resetMocks() {
-        clearAllMocks()
-    }
 }
